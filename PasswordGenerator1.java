3a275d349394c65a244fb05c933001f10eaddf0b
Shashank05101999 <143600573+Shashank05101999@users.noreply.github.com>
2023-11-08 21:19:11 +0530

    Add files via upload

---
+++ PasswordGenerator1.java
@@ -0,0 +1,27 @@
+
+package passwordgenerator;
+
+import java.util.Random;
+
+
+public class PasswordGenerator1 {
+
+    
+    public static void main(String[] args) {
+   
+         String upper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+         String lower ="abcdefghijklmnopqrstuvwxyz";
+         String num="0123456789";
+         String specialChars="<>,.!@#$%^&*(){}[];+=-_";
+         String combination=upper+lower+specialChars+num;
+         int len=8;
+         char[] password=new char[len];
+         Random r=new Random();
+         for(int i=0;i<len;i++)
+         {
+             password[i]=combination.charAt(r.nextInt(combination.length()));
+             
+    }
+        System.out.println(" Generated password is:"+new String(password));
+}
+}
